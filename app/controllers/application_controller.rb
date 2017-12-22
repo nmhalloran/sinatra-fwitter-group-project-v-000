3f1316ee8e9e11e42df105446b8bb3f523ae5c28
Nicholas Halloran <nmhalloran@hotmail.com>
2017-12-22 21:56:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,22 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    @user = User.find(params)
+    erb :'/users/#{@user.id}'
+  end
+
+  get '/signup' do
+    erb :create_user.erb
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    erb :'/users/#{@user.id}'
+  end
+
 end
