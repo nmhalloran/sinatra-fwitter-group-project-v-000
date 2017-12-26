855975fac30019d29fd002c7c72be50892682f00
Nicholas Halloran <nmhalloran@hotmail.com>
2017-12-26 20:40:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    erb :'/users/login'
+    User.is_logged_in?(session) ? (redirect to '/tweets') : (erb :'/users/login')
   end
 
   post '/login' do
@@ -48,7 +48,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    if params[:username].empty? | params[:email].empty?| params[:password].empty?
+    if params[:username].empty? | params[:email].empty? | params[:password].empty?
       redirect to "/users/signup"
     end
     @user = User.new(username: params[:username])
